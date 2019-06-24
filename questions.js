aa6e32eca4f1b7dac99981196bb870df43fe7a8b
Shriya Balgotra <30731005+ShriyaBalgotra@users.noreply.github.com>
2019-06-24 11:16:04 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,86 @@
+module.exports = {
+ 
+  QUESTIONS_EN_US: [
+    {
+      'What is the name of Hagrid’s Dragons?': [
+        'Aragog',
+        'Norbert',
+        'Fang',
+        'Fluffy',
+        
+      ],
+    },
+    {
+      'What was the secret society formed by Dumbledore to oppose Voldemort? ': [
+        'Dumbledore’s Army',
+        'Hogwart’s Champions',
+        'Death Eaters',
+        'The order of the phoenix',
+        
+      ],
+    },
+    {
+      'Pick out the wandmaker from the following:': [
+        'Dragomir Gregorovitch',
+        'Barty Crouch',
+        'Greyback',
+        'Gellert Grindelwald',
+      ],
+    },
+    
+    {
+      'Who is the founder of house Hufflepuff?': [
+        'Rowena Hufflepuff',
+        'Helga Hufflepuff',
+        'Salazar Hufflepuff',
+        'Godric Hufflepuff',
+      ],
+    },
+    {
+      'Which character uses the catch phrase “Constant Vigilance”?': [
+        'Serverus Snape',
+        'Slughorn Horace',
+        'Barty Crouch',
+        'Mad-Eye Moody',
+      ],
+    },
+    {
+      'What is the trait of the Ravenclaw house?': [
+        'Bravery',
+        'Aggressiveness',
+        'Fair and just',
+        'Wit',
+      ],
+    },
+    {
+      'What is the name of the conductor of the Knight Bus?': [
+        'Stan Shunpike',
+        'Ernie Pang',
+        'Amycus Carrow',
+        'Antonin Dolohov',
+      ],
+    },
+    {
+      'How many stairs does Hogwarts have?':[
+        '161',
+        '177',
+        '137',
+        '142'],},
+{'Who does Harry live with?':
+['The Weassley’s','The Black’s ','The Dursley’s','The Granger’s']},
+{'On which floor is the Prefect’s Bathroom?':['5th Floor',' 4th Floor','3rd Floor','2nd Floor']},
+{'What is the shape of Harry’s Patronus?':['Stag','Snake','Doe','Cat']},
+{'Which of these is Hogwart’s highest tower?':[' West Tower','Astronomy Tower','East Tower','Ravenclaw’s Tower']},
+{'Which of these is Hogwart’s highest tower?':['West Tower','Astronomy Tower','East Tower','Ravenclaw’s Tower']},
+{'How does one enter the “Room of Requirements”?':['Using Alohomora','Accidentally','When in need','Using Marauder’s Map']},
+{'What was the occupation of Hermoine Granger’s Parents?':['Engineers','Dentists','Lawyers','Teachers']},
+{'Where did Harry end up when he used the floo powder for the first time?':['Diagon Alley','The Burrow','Hogwarts','Knockturn Alley']},
+{'What did Sirius Black gift Harry?':['Marauder’s Map','Firebolt','Extendable Ears','Invisibility Cloak']},
+{'What Species was Hagrid?':['Human','Inferi','Half-Giant','Werewolf']},
+{'What was the name of Albus Dumbledore’s Brother?':[' Charlie','Peter','Aberforth','Pudifoot']},
+{'Which of the following took care of dragons for a living?':['Charlie',' Bill','Percy','Fred']},
+{'What is the symbol of Ravenclaw House?':['Raven','Crow','Eagle','Hawk']},
+{'What is Dumbledore’s Full name?':['Albus Wulfric Percival Brian Dumbledore','Albus Percival Wulfric Brian Dumbledore','Albus Percival Brain Wulfric Dumbeldore',' Albus Brian Percival Wulfric Dumbledore']},
+  
+]
+};
